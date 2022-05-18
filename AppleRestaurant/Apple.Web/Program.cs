620740bc9066c0620c2a664c95cf64a806a69651
Karthik <56579449+karthik9728@users.noreply.github.com>
2022-05-19 02:36:59 +0530

    Product API Configure in Apple.Web Program.cs File

--- AppleRestaurant/Apple.Web/Program.cs
+++ AppleRestaurant/Apple.Web/Program.cs
@@ -1,7 +1,32 @@
+using Apple.Web;
+using Apple.Web.Services;
+using Apple.Web.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+#region HttpClient Service
+
+//Add Service for Product Service
+builder.Services.AddHttpClient<IProductService,ProductService>();
+
+#endregion
+
+#region SD Url
+
+var connectionString = builder.Configuration["ServiceUrl:ProductAPI"];
+
+SD.ProductAPIBase = connectionString;
+
+#endregion
+
+#region DI Service
+
+builder.Services.AddScoped<IProductService,ProductService>();
+
+#endregion
 
 var app = builder.Build();
 
